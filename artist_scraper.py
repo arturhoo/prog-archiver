bef8305d920bf2214b47a0bbb85dccbf695a9904
Artur Rodrigues <arturhoo@gmail.com>
2014-08-10 11:43:45 -0300

    Scrap artists

---
+++ artist_scraper.py
@@ -0,0 +1,28 @@
+import requests
+from lxml import html
+import re
+import dataset
+
+db = dataset.connect('sqlite:///prog.db')
+ids = db['albums'].distinct('artist_id')
+ids = [id_['artist_id'] for id_ in ids]
+artists_table = db['artists']
+
+for artist_id in ids:
+    base_url = "http://www.progarchives.com/artist.asp?id={0}"
+    url = base_url.format(artist_id)
+    response = requests.get(url)
+    parsed_body = html.fromstring(response.text)
+
+    name = parsed_body.xpath('//div/strong/text()')[0][:-10]
+    genre_and_country = parsed_body.xpath('//h2/text()')[0]
+    regex_match = re.search(ur'^(.+) \u2022 (.+)$', genre_and_country)
+    genre = regex_match.groups()[0]
+    country = regex_match.groups()[1]
+
+    artists_table.insert(dict(
+        id=artist_id,
+        name=name,
+        genre=genre,
+        country=country
+    ))
--- scraper.py
+++ scraper.py
@@ -6,37 +6,39 @@ import dataset
 db = dataset.connect('sqlite:///prog.db')
 albums_table = db['albums']
 
-root_url = 'http://www.progarchives.com/top-prog-albums.asp?syears=2013'
-response = requests.get(root_url)
-parsed_body = html.fromstring(response.text)
-rows = parsed_body.xpath("//table[@cellpadding='7']/tr")
+for year in range(1945, 2015):
+    base_url = "http://www.progarchives.com/top-prog-albums.asp?syears={0}"
+    url = base_url.format(year)
+    response = requests.get(url)
+    parsed_body = html.fromstring(response.text)
+    rows = parsed_body.xpath("//table[@cellpadding='7']/tr")
 
-for row in rows:
-    tds = row.xpath('.//td')
+    for row in rows:
+        tds = row.xpath('.//td')
 
-    rating_str = tds[2].xpath(".//span[contains(@id, 'avgRatings')]/text()")[0]
-    rating = float(rating_str)
-    qwr_text = tds[2].xpath(".//div[contains(@style, '80%')]/text()")[0]
-    qwr = float(re.search(r'(\d.\d+)', qwr_text).groups()[0])
-    ratings = int(tds[2].xpath(".//span[contains(@style, '100%')]/text()")[0])
+        rating_str = tds[2].xpath(".//span[contains(@id, 'avgRatings')]/text()")[0]
+        rating = float(rating_str)
+        qwr_text = tds[2].xpath(".//div[contains(@style, '80%')]/text()")[0]
+        qwr = float(re.search(r'(\d.\d+)', qwr_text).groups()[0])
+        ratings = int(tds[2].xpath(".//span[contains(@style, '100%')]/text()")[0])
 
-    album_id_url = tds[3].xpath(".//a/@href")[0]
-    album_id = int(re.search(r'\?id=(\d+)$', album_id_url).groups()[0])
-    title = tds[3].xpath(".//strong/text()")[0]
-    artist_id_url = tds[3].xpath(".//a/@href")[1]
-    artist_id = int(re.search(r'\?id=(\d+)$', artist_id_url).groups()[0])
-    artist_name = tds[3].xpath(".//a/text()")[0]
+        album_id_url = tds[3].xpath(".//a/@href")[0]
+        album_id = int(re.search(r'\?id=(\d+)$', album_id_url).groups()[0])
+        title = tds[3].xpath(".//strong/text()")[0]
+        artist_id_url = tds[3].xpath(".//a/@href")[1]
+        artist_id = int(re.search(r'\?id=(\d+)$', artist_id_url).groups()[0])
+        artist_name = tds[3].xpath(".//a/text()")[0]
 
-    genre = tds[4].xpath(".//strong/text()")[0]
-    year = int(tds[4].xpath("text()")[0][-4:])
+        genre = tds[4].xpath(".//strong/text()")[0]
+        year = int(tds[4].xpath("text()")[0][-4:])
 
-    albums_table.insert(dict(
-        id=album_id,
-        title=title,
-        artist_id=artist_id,
-        rating=rating,
-        qwr=qwr,
-        ratings=ratings,
-        genre=genre,
-        year=year
-    ))
+        albums_table.insert(dict(
+            id=album_id,
+            title=title,
+            artist_id=artist_id,
+            rating=rating,
+            qwr=qwr,
+            ratings=ratings,
+            genre=genre,
+            year=year
+        ))
